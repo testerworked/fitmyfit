e4a61e17d4b32aac6cfac49aebc36eb96a783b2e
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2022-04-07 20:33:15 +0500

    add project, structure

--- app/src/main/java/com/myfitness/easyfitness/StepCounterActivity.kt
+++ app/src/main/java/com/myfitness/easyfitness/StepCounterActivity.kt
@@ -3,9 +3,9 @@ package com.myfitness.easyfitness
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StepCounterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_step_counter)
     }
 }
---
+++ app/src/main/java/com/myfitness/easyfitness/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.myfitness.easyfitness.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myfitness.easyfitness.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
