2132c23553e5e351e92e3b157ddafb3a3ec2595a
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2022-04-12 08:58:05 +0500

    change: business logic;

---
+++ app/src/main/java/com/myfitness/easyfitness/domain/Calendar.kt
@@ -0,0 +1,10 @@
+package com.myfitness.easyfitness.domain
+
+import java.util.*
+
+
+data class Calendar(
+    val eventExercise: String,
+    val day: Int,
+    val date: Date
+)
---
+++ app/src/main/java/com/myfitness/easyfitness/domain/StepCounter.kt
@@ -0,0 +1,7 @@
+package com.myfitness.easyfitness.domain
+
+data class StepCounter(
+    val oneStep: Int,
+    val manyStep: Int,
+    val todayStep: Int
+)
