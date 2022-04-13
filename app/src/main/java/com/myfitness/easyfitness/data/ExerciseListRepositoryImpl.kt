581fcfc9fe706f9610c3ca134e3a1584829e53f9
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2022-04-13 15:49:13 +0500

    edit: impl methods;

--- app/src/main/java/com/myfitness/easyfitness/data/ExerciseListRepositoryImpl.kt
+++ app/src/main/java/com/myfitness/easyfitness/data/ExerciseListRepositoryImpl.kt
@@ -2,25 +2,39 @@ package com.myfitness.easyfitness.data
 
 import com.myfitness.easyfitness.domain.ExerciseItem
 import com.myfitness.easyfitness.domain.ExerciseListRepository
+import java.lang.RuntimeException
 
 object ExerciseListRepositoryImpl: ExerciseListRepository {
+
+    private val exerciseList = mutableListOf<ExerciseItem>()
+
+    private var autoIncrementId = 0
+
     override fun addExerciseItem(exerciseItem: ExerciseItem) {
-        TODO("Not yet implemented")
+        if(exerciseItem.id == ExerciseItem.UNDEFINED_ID){
+            exerciseItem.id = autoIncrementId++
+        }
+
+        exerciseList.add(exerciseItem)
     }
 
     override fun deleteExerciseItem(exerciseItem: ExerciseItem) {
-        TODO("Not yet implemented")
+        exerciseList.remove(exerciseItem)
     }
 
     override fun editExerciseItem(exerciseItem: ExerciseItem) {
-        TODO("Not yet implemented")
+        val oldElement = getExerciseItem(exerciseItem.id)
+        exerciseList.remove(oldElement)
+        addExerciseItem(exerciseItem)
     }
 
     override fun getExerciseItem(exerciseItemId: Int): ExerciseItem {
-        TODO("Not yet implemented")
+        return exerciseList.find {
+            it.id == exerciseItemId
+        } ?: throw RuntimeException("Element with id $exerciseItemId not found")
     }
 
     override fun getExerciseList(): List<ExerciseItem> {
-        TODO("Not yet implemented")
+        return exerciseList.toList()
     }
 }
