9f137f0cbf328a04ded43c98130c82f58b56352c
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2022-04-13 15:25:27 +0500

    add: impl methods in data object;

--- app/src/main/java/com/myfitness/easyfitness/data/ExerciseListRepositoryImpl.kt
+++ app/src/main/java/com/myfitness/easyfitness/data/ExerciseListRepositoryImpl.kt
@@ -1,4 +1,26 @@
 package com.myfitness.easyfitness.data
 
-object ExerciseListRepositoryImpl {
+import com.myfitness.easyfitness.domain.ExerciseItem
+import com.myfitness.easyfitness.domain.ExerciseListRepository
+
+object ExerciseListRepositoryImpl: ExerciseListRepository {
+    override fun addExerciseItem(exerciseItem: ExerciseItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteExerciseItem(exerciseItem: ExerciseItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editExerciseItem(exerciseItem: ExerciseItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExerciseItem(exerciseItemId: Int): ExerciseItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExerciseList(): List<ExerciseItem> {
+        TODO("Not yet implemented")
+    }
 }
