1e3e2523dea2e93f328a60eb4355d8e2e7dd69d3
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2022-04-13 16:58:26 +0500

    version: 1.2:3; code review;

--- app/src/main/java/com/myfitness/easyfitness/presentation/MainActivity.kt
+++ app/src/main/java/com/myfitness/easyfitness/presentation/MainActivity.kt
@@ -2,13 +2,23 @@ package com.myfitness.easyfitness.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.myfitness.easyfitness.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    val TAG: String = "fitmyfit"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.exerciseList.observe(this){
+            Log.d(TAG, it.toString())
+        }
 
     }
 
--- app/src/main/java/com/myfitness/easyfitness/presentation/MainViewModel.kt
+++ app/src/main/java/com/myfitness/easyfitness/presentation/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.myfitness.easyfitness.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.myfitness.easyfitness.data.ExerciseListRepositoryImpl
 import com.myfitness.easyfitness.domain.DeleteExerciseItemUseCase
 import com.myfitness.easyfitness.domain.EditExerciseItemUseCase
+import com.myfitness.easyfitness.domain.ExerciseItem
 import com.myfitness.easyfitness.domain.GetExerciseListUseCase
 
 class MainViewModel: ViewModel() {
@@ -13,4 +15,13 @@ class MainViewModel: ViewModel() {
     private val getExerciseListUseCase = GetExerciseListUseCase(repository)
     private val deleteExerciseItemUseCase = DeleteExerciseItemUseCase(repository)
     private val editExerciseItemUseCase = EditExerciseItemUseCase(repository)
+
+
+    val exerciseList = MutableLiveData<List<ExerciseItem>>()
+
+    fun getExerciseList(){
+        val list = getExerciseListUseCase.getExerciseList()
+        exerciseList.value = list
+    }
+
 }
