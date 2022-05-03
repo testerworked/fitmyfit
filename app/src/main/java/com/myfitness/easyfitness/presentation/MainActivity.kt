bb2a98d0667c6e4a8fd62d26891903f3ad1a9aaf
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2022-05-03 10:02:29 +0500

    fix: output exerciseItems from linearlayout;

--- app/src/main/java/com/myfitness/easyfitness/presentation/MainActivity.kt
+++ app/src/main/java/com/myfitness/easyfitness/presentation/MainActivity.kt
@@ -3,8 +3,12 @@ package com.myfitness.easyfitness.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.myfitness.easyfitness.R
+import com.myfitness.easyfitness.domain.ExerciseItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,21 +16,42 @@ class MainActivity : AppCompatActivity() {
     val TAG: String = "fitmyfit"
 
     private var count = 0
+    private lateinit var llExerciseList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        llExerciseList = findViewById(R.id.ll_exercise_list)
+
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.exerciseList.observe(this){
             Log.d(TAG, it.toString())
 
-            if(count == 0){
-                count++
-                val item = it[0]
-                viewModel.changeEnableState(item)
-            }
+//            if(count == 0){
+//                count++
+//                val item = it[0]
+//                viewModel.changeEnableState(item)
+//            }
+            showList(it)
+        }
 
+    }
+
+    private fun showList(list: List<ExerciseItem>){
+
+        for(exerciseItem in list){
+            val layoutId = if(exerciseItem.enabled){
+                R.layout.item_exercise_enabled
+            }else{
+                R.layout.item_exercise_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId, llExerciseList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = exerciseItem.name
+            tvCount.text = exerciseItem.count.toString()
+            llExerciseList.addView(view)
         }
 
     }
